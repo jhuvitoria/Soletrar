e8ca14413c15f23d96c20a75d80a4e9a2450d510
jhu vitoria <jhuliavitoria226@gmail.com>
2024-04-08 14:08:49 -0300

    Exercício concluído com exito!

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Soletrar");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.Write("DIGITE UMA PALAVRA: ");
+Console.ResetColor();
+string PALAVRA = Console.ReadLine()!;
+var caracteres = PALAVRA.ToCharArray();
+string palavraSoletrada = String.Join('-', caracteres).ToUpper(); 
+Console.WriteLine($"\nResultado: {palavraSoletrada}");
+
+
+
+
+
+
+
